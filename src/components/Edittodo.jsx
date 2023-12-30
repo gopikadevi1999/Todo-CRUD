54c7f21777eb4af5130f3f61771d9505945d6f9f
gopikadevi1999 <gopikadevi6363@gmail.com>
2023-12-31 01:10:44 +0530

    edit updated

--- src/components/Edittodo.jsx
+++ src/components/Edittodo.jsx
@@ -6,10 +6,10 @@ function Edittodo({ title, setTitle, description, setDescription, onClick }) {
         <div className='edittodo'>
             <div className="row">
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="Todo Name" value={title} onChange={() => { setTitle(e.target.value) }} />
+                    <input type="text" className="form-control" placeholder="Todo Name" value={title} onChange={(e) => { setTitle(e.target.value) }} />
                 </div>
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="Todo Description" value={description} onChange={() => { setDescription(e.target.value) }} />
+                    <input type="text" className="form-control" placeholder="Todo Description" value={description} onChange={(e) => { setDescription(e.target.value) }} />
                 </div>
                 <div className="col">
                     <button type="button" className="btn btn-info" onClick={onClick} >Save Changes</button>
